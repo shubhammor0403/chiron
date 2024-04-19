9631e82f3d8cbaef3b9f065cdc3f172679fa29cc
Shubham Mor <shubhammor@outlook.com>
2024-04-19 22:34:39 +0530

    middleware

--- chiron/settings.py
+++ chiron/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    "django.middleware.static.StaticFilesMiddleware",
+    'django.contrib.staticfiles.middleware.StaticFilesMiddleware',
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
