b9c50f2dfbe903e602f3e0cd1cd365a765f53b0d
Shubham Mor <shubhammor@outlook.com>
2024-04-19 20:44:50 +0530

    settings

--- chiron/settings.py
+++ chiron/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-hyx#%)tucs%o)u3oyga$^5o%d8ru!yfu2nw!(4ib_he62yau=2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
